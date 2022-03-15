28b8044ec7135bdc8282550da27537d9e54215dc
cosmologysk8 <a1999afg@gmail.com>
2022-03-15 17:05:44 +0100

    dia 15 marzo

--- src/main/java/utilidades/UtilidadesPartida.java
+++ src/main/java/utilidades/UtilidadesPartida.java
@@ -4,9 +4,14 @@ import modelos.Jugador;
 import modelos.Partida;
 import modelos.Personaje;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UtilidadesPartida implements IUtilidadesPartida{
 
@@ -27,28 +32,44 @@ public class UtilidadesPartida implements IUtilidadesPartida{
                         break;
                     }
                 }
-
             }
        }
 
-       for (Jugador jg : participantes){
-           int team = (int) Math.floor(Math.random());
+       HashSet<Jugador> jugadores_equipo_1 = new HashSet<>();
+       HashSet<Jugador> jugadores_equipo_2 = new HashSet<>();
+
+       while (jugadores_equipo_1.size() < 5 && jugadores_equipo_2.size() < 5){
+           for (Jugador j : participantes){
+               jugadores_equipo_1.add(j);
+               jugadores_equipo_2.add(j);
+           }
        }
 
-       partida.getJugadoresPorEquipo().put(1,null);
-       partida.getJugadoresPorEquipo().put(2, null);
+        partida.getJugadoresPorEquipo().put(1,jugadores_equipo_1);
+        partida.getJugadoresPorEquipo().put(2, jugadores_equipo_2);
 
-        for (Jugador j : participantes){
-            int eleccion_equipo = (int) Math.floor(Math.random()*1+2);
-            partida.setJugadoresPorEquipo();
-
-        }
-
-        LocalDateTime incio = partida.getInicioPartida();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime inicio = LocalDateTime.parse(dtf.format(LocalDateTime.now()));
+        partida.setInicioPartida(inicio);
     }
 
     @Override
     public void finalizarPartida(Partida partida, Integer equipoVencedor) {
-        LocalDateTime fin = partida.getFinPartida();
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime fin = LocalDateTime.parse(dtf.format(LocalDateTime.now()));
+        partida.setFinPartida(fin);
+
+        // Duration duracion = Duration.between(partida.getInicioPartida(), partida.getFinPartida());
+        // duracion.getSeconds();
+
+        long duracion_2 = partida.getInicioPartida().until(partida.getFinPartida(), ChronoUnit.SECONDS);
+        int duracion_total = (int) duracion_2;
+        partida.setDuracionPartida(duracion_total);
+
+        partida.setEquipoVencedor(equipoVencedor);
+
+
+
     }
 }
